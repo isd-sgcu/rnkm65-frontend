af2583d532be03bdf3540c1416061f9bce850e15
PKhing <apimrak@gmail.com>
2022-03-25 17:08:39 +0700

    ✨ Add Typography

---
+++ common/components/Typography/styled.ts
@@ -0,0 +1,70 @@
+import { styled } from 'config/stitches.config'
+
+export const Typography = styled('div', {
+  fontFamily: '$FCSubjectRounded',
+  variants: {
+    variant: {
+      h1: {
+        fontSize: '4rem',
+        fontWeight: 'bold',
+        '@sm': {
+          fontSize: '2rem',
+        },
+      },
+      h2: {
+        fontSize: '3rem',
+        fontWeight: 'bold',
+        '@sm': {
+          fontSize: '1.75rem',
+        },
+      },
+      h3: {
+        fontSize: '2.25rem',
+        fontWeight: 'bold',
+        '@sm': {
+          fontSize: '1.5rem',
+        },
+      },
+      h4: {
+        fontSize: '1.5rem',
+        fontWeight: 'bold',
+        '@sm': {
+          fontSize: '1rem',
+        },
+      },
+      subhead1: {
+        fontSize: '2.5rem',
+        '@sm': {
+          fontSize: '1.5rem',
+        },
+      },
+      subhead2: {
+        fontSize: '2rem',
+        '@sm': {
+          fontSize: '1.125rem',
+        },
+      },
+      subhead3: {
+        fontSize: '1.5rem',
+        '@sm': {
+          fontSize: '1rem',
+        },
+      },
+      body: {
+        fontSize: '1rem',
+        '@sm': {
+          fontSize: '0.75rem',
+        },
+      },
+      description: {
+        fontSize: '0.75rem',
+        '@sm': {
+          fontSize: '0.625rem',
+        },
+      },
+    },
+  },
+  defaultVariants: {
+    variant: 'body',
+  },
+})
