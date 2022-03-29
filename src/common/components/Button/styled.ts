446ac6cdc6ca6748ab90120f1c410a4f7d8a7bb5
PKhing <apimrak@gmail.com>
2022-03-29 22:15:30 +0700

    💄 Edit button component

--- src/common/components/Button/styled.ts
+++ src/common/components/Button/styled.ts
@@ -1,23 +1,41 @@
-import { styled } from '@stitches/react'
+import { styled } from 'config/stitches.config'
 
 export const Button = styled('button', {
   fontSize: '1.5rem',
   fontWeight: 'bold',
   fontFamily: '$FCSubjectRounded',
 
-  backgroundColor: '$pink500',
-  color: '$yellow',
-
   border: 'none',
   borderRadius: '16px',
   padding: '4px 14px 2px 14px',
 
   cursor: 'pointer',
+  boxShadow: '2px 2px 2px 0px #24066840',
 
   '@sm': {
     fontSize: '1rem',
+    padding: '3px 10px 2px 10px',
   },
 
-  '&:hover': { backgroundColor: '$pink600' },
-  '&:disabled': { backgroundColor: '$pink400' },
+  variants: {
+    variant: {
+      primary: {
+        color: '$yellow',
+        backgroundColor: '$pink500',
+        '&:hover': { backgroundColor: '$pink600' },
+        '&:disabled': { backgroundColor: '$pink400' },
+      },
+      secondary: {
+        color: '$pink500',
+        backgroundColor: '$yellow',
+      },
+      error: {
+        color: '$white',
+        backgroundColor: '$error',
+      },
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+  },
 })
