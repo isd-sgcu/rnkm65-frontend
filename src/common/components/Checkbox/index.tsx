fd58d76e676310bcddf22ecebf711d2bea56e106
miello <partearth1212@gmail.com>
2022-03-30 16:39:03 +0700

    ⚡️ Wrap base component with memo

--- src/common/components/Checkbox/index.tsx
+++ src/common/components/Checkbox/index.tsx
@@ -1,17 +1,21 @@
+import { forwardRef, memo } from 'react'
+
 import { Checkmark, InputBox, InputLabel, RootCheckbox } from './styled'
 import { ICheckboxProps } from './types'
 
-const Checkbox = (props: ICheckboxProps) => {
-  const { label, ...remain } = props
-  return (
-    <RootCheckbox>
-      <InputLabel>
-        {label}
-        <InputBox type="checkbox" {...remain} />
-        <Checkmark />
-      </InputLabel>
-    </RootCheckbox>
-  )
-}
+const Checkbox = memo(
+  forwardRef<HTMLInputElement, ICheckboxProps>((props, ref) => {
+    const { label, ...remain } = props
+    return (
+      <RootCheckbox>
+        <InputLabel>
+          {label}
+          <InputBox type="checkbox" {...remain} ref={ref} />
+          <Checkmark />
+        </InputLabel>
+      </RootCheckbox>
+    )
+  })
+)
 
 export default Checkbox
