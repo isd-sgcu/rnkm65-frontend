6fad29b1e941da50571b3268f7ab8c31762248a9
miello <partearth1212@gmail.com>
2022-03-31 23:33:29 +0700

    ✨ Add instance for each request type

--- src/common/utils/axios.ts
+++ src/common/utils/axios.ts
@@ -1,9 +1,32 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 import { API_BASE_URL } from 'config/env'
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 3000,
+  timeout: 10000,
+  withCredentials: true,
 })
 
-export { apiClient }
+const httpGet = <T = any>(
+  url: string,
+  config: AxiosRequestConfig<any>
+): Promise<AxiosResponse<T>> => apiClient.get(url, config)
+
+const httpPost = <T = any, U = any>(
+  url: string,
+  body: T,
+  config: AxiosRequestConfig<any>
+): Promise<AxiosResponse<U>> => apiClient.post(url, body, config)
+
+const httpPut = <T = any, U = any>(
+  url: string,
+  body: T,
+  config: AxiosRequestConfig<any>
+): Promise<AxiosResponse<U>> => apiClient.put(url, body, config)
+
+const httpDelete = <T = any>(
+  url: string,
+  config: AxiosRequestConfig<any>
+): Promise<AxiosResponse<T>> => apiClient.delete(url, config)
+
+export { apiClient, httpDelete, httpGet, httpPost, httpPut }
