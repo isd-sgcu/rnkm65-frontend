55b96e523a9689713dbf44003cdc061ecc945f99
miello <partearth1212@gmail.com>
2022-04-13 21:58:28 +0700

    🐛 Fix optional config parameter

--- src/common/utils/axios.ts
+++ src/common/utils/axios.ts
@@ -9,24 +9,24 @@ const apiClient = axios.create({
 
 const httpGet = <T = any>(
   url: string,
-  config: AxiosRequestConfig<any>
+  config?: AxiosRequestConfig<any>
 ): Promise<AxiosResponse<T>> => apiClient.get(url, config)
 
 const httpPost = <T = any, U = any>(
   url: string,
   body: T,
-  config: AxiosRequestConfig<any>
+  config?: AxiosRequestConfig<any>
 ): Promise<AxiosResponse<U>> => apiClient.post(url, body, config)
 
 const httpPut = <T = any, U = any>(
   url: string,
   body: T,
-  config: AxiosRequestConfig<any>
+  config?: AxiosRequestConfig<any>
 ): Promise<AxiosResponse<U>> => apiClient.put(url, body, config)
 
 const httpDelete = <T = any>(
   url: string,
-  config: AxiosRequestConfig<any>
+  config?: AxiosRequestConfig<any>
 ): Promise<AxiosResponse<T>> => apiClient.delete(url, config)
 
 export { apiClient, httpDelete, httpGet, httpPost, httpPut }
