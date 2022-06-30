4e31b9ab9617d26c9777326c7ff68c3a68eb1b74
Peam <peammy2010@hotmail.com>
2022-06-30 18:34:51 +0700

    late page

---
+++ src/module/LatePage/index.tsx
@@ -0,0 +1,13 @@
+import { Box, Container, Title } from './styled'
+
+const LatePage = () => (
+  <Container>
+    <Box>
+      <Title>ขออภัย !</Title>
+      หมดช่วงของการลงทะเบียนแล้ว ขอบคุณที่ให้ความสนใจโครงการรับเพื่อนก้าวใหม่
+      สามารถสอบถามข้อมูลเพิ่มเติมได้ที่ CU for Freshmen
+    </Box>
+  </Container>
+)
+
+export default LatePage
---
+++ src/pages/latePage.tsx
@@ -0,0 +1,12 @@
+import LatePage from 'module/LatePage'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+export async function getServerSideProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'profile'])),
+      // Will be passed to the page component as props
+    },
+  }
+}
+export default LatePage
