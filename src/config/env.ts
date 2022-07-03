657b49496f4f45e7a6d342cb933b791ae0330160
Thanayut T <b@thanayut.in.th>
2022-07-03 19:54:19 +0700

    Adjust config for deploy preview

--- src/config/env.ts
+++ src/config/env.ts
@@ -9,7 +9,8 @@ export const API_BASE_URL =
   publicRuntimeConfig.env.API_BASE_URL || 'https://pdev.freshersfairs.com'
 
 export const APP_BASE_URL =
-  publicRuntimeConfig.env.APP_BASE_URL || 'http://localhost:3000'
+  publicRuntimeConfig.env.APP_BASE_URL ||
+  'https://deploy-preview-14--cerulean-sable-b1ec76.netlify.app'
 
 export const SSO_BASE_URL =
   publicRuntimeConfig.env.SSO_BASE_URL || 'http://sso.cucheck.in'
