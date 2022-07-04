3bb635c751a6d7664ca2d0f9e85be4e9097e9cd3
Thanayut T <b@thanayut.in.th>
2022-07-04 16:40:04 +0700

    Update user type interfaces

--- src/common/types/user.ts
+++ src/common/types/user.ts
@@ -1,5 +1,18 @@
 export interface IUser {
+  id: string
+  studentID: string
+  faculty: string
+  year: string
   firstname: string
   lastname: string
+  nickname: string
+  email: string
+  phone: string
+  lineID: string
+  disease: string
+  allergyFood: string
+  allergyMedicine: string
+  foodRestriction: string
   imageUrl: string
+  vaccineCertificateUrl: string
 }
