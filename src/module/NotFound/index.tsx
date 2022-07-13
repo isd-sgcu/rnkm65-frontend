a155d5704c7d047bef78b53ccc4ea38e80ea9e0e
miello <partearth1212@gmail.com>
2022-07-14 01:34:05 +0700

    Apply i18n to not found page

--- src/module/NotFound/index.tsx
+++ src/module/NotFound/index.tsx
@@ -1,17 +1,19 @@
 import Button from 'common/components/Button'
 import Typography from 'common/components/Typography'
+import useSSRTranslation from 'common/hooks/useSSRTranslation'
 import { useRouter } from 'next/router'
 import React from 'react'
 
 import { Container } from './styled'
 
 const NotFound = () => {
+  const { t } = useSSRTranslation('common')
   const router = useRouter()
   return (
     <Container>
-      <Typography variant="h3">ไม่พบหน้าที่ค้นหา</Typography>
+      <Typography variant="h3">{t('notFoundPage')}</Typography>
       <Button type="button" onClick={() => router.push('/')}>
-        กลับ
+        {t('back')}
       </Button>
     </Container>
   )
