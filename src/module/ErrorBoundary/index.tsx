0cabed2697b7aa92501c10c349a542ddab9e3d91
Pimrak Apiratchatanont <apimrak@gmail.com>
2022-07-14 22:44:12 +0700

    Add report issue form prefill

--- src/module/ErrorBoundary/index.tsx
+++ src/module/ErrorBoundary/index.tsx
@@ -3,18 +3,29 @@ import Typography from 'common/components/Typography'
 import useSSRTranslation from 'common/hooks/useSSRTranslation'
 import { useRouter } from 'next/router'
 import React from 'react'
+import { FallbackProps } from 'react-error-boundary'
 
 import { Container } from './styled'
 
-const ErrorFallback = () => {
+const ErrorFallback = ({ error }: FallbackProps) => {
   const { t } = useSSRTranslation('common')
   const router = useRouter()
+
+  const handleReportIssue = () => {
+    window.location.href = `https://airtable.com/shrWFil4igZa2UZoV?prefill_errorMessage=${error}&hide_errorMessage=true`
+  }
+
   return (
     <Container>
       <Typography variant="h3">{t('someErrorHappen')}</Typography>
-      <Button type="button" onClick={() => router.push('/')}>
-        {t('back')}
-      </Button>
+      <div style={{ display: 'flex', gap: '20px' }}>
+        <Button type="button" onClick={handleReportIssue}>
+          {t('reportIssue')}
+        </Button>
+        <Button type="button" onClick={() => router.push('/')}>
+          {t('back')}
+        </Button>
+      </div>
     </Container>
   )
 }
