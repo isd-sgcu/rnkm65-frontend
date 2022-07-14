61528d1dc42f201bbdeaae6ede242fc079ddd341
miello <partearth1212@gmail.com>
2022-07-14 23:01:36 +0700

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,6 +17,23 @@ export default class Document extends NextDocument {
             // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{ __html: getCssText() }}
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
         </Head>
         <body>
           <Main />
