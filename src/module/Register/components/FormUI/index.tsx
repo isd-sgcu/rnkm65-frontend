5366a5a4d79e6023367a941e57c1e7bb9959f214
miello <partearth1212@gmail.com>
2022-07-16 14:34:21 +0700

    Add link for who does not have certificate

--- src/module/Register/components/FormUI/index.tsx
+++ src/module/Register/components/FormUI/index.tsx
@@ -2,10 +2,11 @@ import Typography from 'common/components/Typography'
 import { useAuth } from 'common/contexts/AuthContext'
 import useSSRTranslation from 'common/hooks/useSSRTranslation'
 import { templateForm } from 'module/Register/utils/schema'
+import { Trans } from 'next-i18next'
 import { Fragment, memo } from 'react'
 
 import InnerFormController from './components/InnerFormController'
-import { FormContainer, FormRootContainer } from './styled'
+import { FormContainer, FormLink, FormRootContainer } from './styled'
 
 const FormUI = memo(() => {
   const { t } = useSSRTranslation('register')
@@ -15,6 +16,20 @@ const FormUI = memo(() => {
     <FormRootContainer>
       <Typography color="new-secondary" variant="h3">
         {t('annoucement')}
+      </Typography>
+      <Typography color="new-primary" variant="body">
+        <Trans
+          i18nKey="register:notRegisterMorProm"
+          components={[
+            // eslint-disable-next-line jsx-a11y/anchor-has-content
+            <FormLink
+              aria-label="non-vaccine-form"
+              href="https://forms.gle/qSv6DCvgRGQkCtEK9"
+              target="_blank"
+              rel="noopener noreferrer"
+            />,
+          ]}
+        />
       </Typography>
       {templateForm.map((row, idx) => (
         <Fragment key={JSON.stringify(row)}>
