1dccb87c27b6231d4e0083950b88ea6162181698
Pimrak Apiratchatanont <apimrak@gmail.com>
2022-07-16 16:45:46 +0700

    Add env to bypass waiting page

--- next.config.js
+++ next.config.js
@@ -11,6 +11,7 @@ const nextConfig = {
       API_BASE_URL: process.env.API_BASE_URL,
       APP_BASE_URL: process.env.APP_BASE_URL,
       SSO_BASE_URL: process.env.SSO_BASE_URL,
+      ACCESS_PROFILE: process.env.ACCESS_PROFILE,
     },
   },
 }
