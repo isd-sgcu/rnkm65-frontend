642cee933cd01eb0f6a72917a5d4fd0a51e16a00
miello <partearth1212@gmail.com>
2022-07-18 01:02:52 +0700

    Change baan selection date

--- src/common/utils/date.ts
+++ src/common/utils/date.ts
@@ -1,7 +1,7 @@
 import { ACCESS_PROFILE } from 'config/env'
 
-const BAAN_SELECTION_START = new Date('July 21, 2022 12:01 GMT+07:00').getTime()
-const BAAN_SELECTION_END = new Date('July 25, 2022 12:00 GMT+07:00').getTime()
+const BAAN_SELECTION_START = new Date('July 21, 2022 09:00 GMT+07:00').getTime()
+const BAAN_SELECTION_END = new Date('July 25, 2022 20:00 GMT+07:00').getTime()
 
 export const getDateStatus = () => {
   const now = new Date().getTime()
