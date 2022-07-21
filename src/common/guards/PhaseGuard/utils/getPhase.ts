636b10a5808b7ebf5a31f88df9e8125e56ecc3e0
Pimrak Apiratchatanont <apimrak@gmail.com>
2022-07-22 01:11:27 +0700

    Add phase to env

--- src/common/guards/PhaseGuard/utils/getPhase.ts
+++ src/common/guards/PhaseGuard/utils/getPhase.ts
@@ -1,6 +1,12 @@
-import { PHASE_DATA } from '../constants'
+import { Phase, PHASE_DATA } from '../constants'
 
-const getPhase = (currentDate: Date) =>
-  PHASE_DATA.find(({ startTime }) => currentDate.getTime() >= startTime)!.phase
+const getPhase = (currentDate: Date) => {
+  if (process.env.NODE_ENV !== 'production' && process.env.NEXT_PUBLIC_PHASE) {
+    return Phase[process.env.NEXT_PUBLIC_PHASE as keyof typeof Phase]
+  }
+
+  return PHASE_DATA.find(({ startTime }) => currentDate.getTime() >= startTime)!
+    .phase
+}
 
 export default getPhase
