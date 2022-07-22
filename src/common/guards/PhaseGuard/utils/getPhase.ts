840bf65ce1a58f1524107d25615d593139c6367d
Pimrak Apiratchatanont <apimrak@gmail.com>
2022-07-22 11:08:41 +0700

    Remove node env check for phase override

--- src/common/guards/PhaseGuard/utils/getPhase.ts
+++ src/common/guards/PhaseGuard/utils/getPhase.ts
@@ -1,7 +1,7 @@
 import { Phase, PHASE_DATA } from '../../../constants/phase'
 
 const getPhase = (currentDate: Date) => {
-  if (process.env.NODE_ENV !== 'production' && process.env.NEXT_PUBLIC_PHASE) {
+  if (process.env.NEXT_PUBLIC_PHASE) {
     return Phase[process.env.NEXT_PUBLIC_PHASE as keyof typeof Phase]
   }
 
