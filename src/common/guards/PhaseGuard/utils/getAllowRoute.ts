92446bbd843d0a5c5eeef9128f621957b39ae0b2
Pimrak Apiratchatanont <apimrak@gmail.com>
2022-07-22 11:42:19 +0700

    Fix BYPASS phase bug

--- src/common/guards/PhaseGuard/utils/getAllowRoute.ts
+++ src/common/guards/PhaseGuard/utils/getAllowRoute.ts
@@ -3,7 +3,7 @@ import { DEFAULT_ROUTE, Phase, PHASE_DATA } from '../../../constants/phase'
 const getAllowRoute = (phase: Phase) => {
   const { allowRoute } = PHASE_DATA.find(
     (phaseData) => phaseData.phase === phase
-  )!
+  ) || { allowRoute: [] }
 
   return DEFAULT_ROUTE.concat(allowRoute)
 }
