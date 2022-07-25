cb2d6644a280fa6ad37ec60e4eb84085786cf47d
NakZaa <nak11494@gmail.com>
2022-07-25 14:24:07 +0700

    Added Back Button

--- src/common/components/QR/index.tsx
+++ src/common/components/QR/index.tsx
@@ -1,34 +1,47 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { useState, useCallback } from 'react'
 import { QrReader } from 'react-qr-reader'
 import { CameraBg } from './camerabg'
-import { Camera, CameraContainer } from './styled'
+import { BackButton, Camera, CameraContainer, QRcontainer } from './styled'
 
 const Qr = () => {
   const [data, setData] = useState('No Result')
 
+  const router = useRouter()
+
+  const handleBackClick = useCallback(() => {
+    router.push('/')
+  }, [router])
+
   return (
-    <CameraContainer>
-      <Camera>
-        <QrReader
-          scanDelay={300}
-          onResult={(result) => {
-            if (result) {
-              setData(result.getText())
-            }
-          }}
-          constraints={{ facingMode: 'environment' }}
-          videoContainerStyle={{
-            position: 'relative',
-          }}
-          containerStyle={{ width: '100%', height: '100%' }}
-          videoStyle={{
-            width: '100%',
-          }}
-          ViewFinder={CameraBg}
-        />
-      </Camera>
-    </CameraContainer>
+    <QRcontainer>
+      
+      <CameraContainer>
+        <Camera>
+        <BackButton onClick={handleBackClick}>X</BackButton>
+          <QrReader
+            scanDelay={300}
+            onResult={(result) => {
+              if (result) {
+                setData(result.getText())
+              }
+            }}
+            constraints={{ facingMode: 'environment' }}
+            videoContainerStyle={{
+              position: 'relative',
+            }}
+            containerStyle={{ width: '100%', height: '100%' }}
+            videoStyle={{
+              width: 'initial',
+              paddingBottom: '5px',
+              paddingTop: '1px'
+            }}
+            ViewFinder={CameraBg}
+          />
+        </Camera>
+      </CameraContainer>
+    </QRcontainer>
   )
 }
 
--- src/common/components/QR/styled.ts
+++ src/common/components/QR/styled.ts
@@ -1,5 +1,14 @@
 import { styled } from 'config/stitches.config'
 
+export const QRcontainer = styled('div', {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%'
+})
+
 export const Camera = styled('div', {
     width : '100%',
     height : '100%',
@@ -16,7 +25,36 @@ export const CameraContainer = styled('div', {
     height : '100%',
     display : 'flex',
     alignItems : 'center',
-    backgroundColor : '$white',
     position : 'relative',
     
+})
+
+//button to go back to previous page
+export const BackButton = styled('button', {
+    position : 'absolute',
+    right : '15px',
+    top : '15px',
+    width : '30px',
+    height : '30px',
+    backgroundColor : '$white',
+    borderRadius: '0.5rem',
+    border : 'none',
+    outline : 'none',
+    cursor : 'pointer',
+    zIndex : '51',
+    filter: 'drop-shadow(0 1px 1px rgb(0 0 0 / 0.05))',
+})
+
+//button to go back to come to QR page
+export const CameraButton = styled('button', {
+    position : 'absolute',
+    bottom : '0',
+    right : '0',
+    width : '100%',
+    height : '100%',
+    backgroundColor : '$white',
+    border : 'none',
+    outline : 'none',
+    cursor : 'pointer',
+    zIndex : '1',
 })
