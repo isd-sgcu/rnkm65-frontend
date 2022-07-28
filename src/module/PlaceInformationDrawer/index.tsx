7aeba421bc02aa19fd41575ad4cab498f6fca9d3
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2022-07-29 02:40:11 +0700

    Fix text overflow problem

--- src/module/PlaceInformationDrawer/index.tsx
+++ src/module/PlaceInformationDrawer/index.tsx
@@ -1,12 +1,14 @@
 import Button from 'common/components/Button'
 import Image from 'common/components/Image'
 import Typography from 'common/components/Typography'
+import useHideFooter from 'common/contexts/LayoutContext/hooks/useHideFooter'
 import useSSRTranslation from 'common/hooks/useSSRTranslation'
 import React, { useCallback, useState } from 'react'
 
 import {
   Box,
   CloseButtonContainer,
+  ImageContainer,
   PlaceDetailsContainer,
   TextBox,
 } from './styled'
@@ -22,6 +24,7 @@ const PlaceInformationDrawer = ({
   timeEn,
 }: PlaceInformationDrawerProps) => {
   const { i18n, t } = useSSRTranslation('placeInformationDrawer')
+  useHideFooter()
 
   const [checkInBtn, setCheckInBtn] = useState(false)
   const checkInBtnClickHandler = useCallback(() => {
@@ -36,7 +39,9 @@ const PlaceInformationDrawer = ({
       <Typography variant="h3" color="new-primary">
         {i18n.language === 'en' ? titleEn : title}
       </Typography>
-      <Image src={imgUrl} width={500} height={250} />
+      <ImageContainer>
+        <Image src={imgUrl} width={500} height={250} />
+      </ImageContainer>
       <PlaceDetailsContainer>
         <TextBox>
           <Typography color="new-primary">
--- src/module/PlaceInformationDrawer/styled.ts
+++ src/module/PlaceInformationDrawer/styled.ts
@@ -5,21 +5,45 @@ export const Box = styled('div', {
   borderRadius: '30px 30px 0 0',
   boxShadow: '0px -1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06)',
   height: 'calc(100vh - 106px)',
-  maxWidth: '420px',
+  maxWidth: '500px',
   background: '$white',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   rowGap: '1.25rem',
   padding: '3.5rem 0',
+  '@md': {
+    height: 'calc(100vh - 96px)',
+  },
   '@sm': {
     height: 'calc(100vh - 68px)',
   },
 })
 
+export const CloseButtonContainer = styled('div', {
+  position: 'absolute',
+  top: '20px',
+  right: '20px',
+  cursor: 'pointer',
+})
+
+export const ImageContainer = styled('div', {
+  flexShrink: '0',
+})
+
+export const PlaceDetailsContainer = styled('div', {
+  margin: '0 2rem',
+  display: 'flex',
+  height: '100%',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  overflow: 'hidden',
+  rowGap: '2.5rem',
+})
+
 export const TextBox = styled('div', {
   overflowY: 'auto',
-  width: 'fit-content',
+  padding: '0 4px',
   '&::-webkit-scrollbar': {
     width: '4px',
   },
@@ -31,21 +55,4 @@ export const TextBox = styled('div', {
     background: '#D1D5DB',
     borderRadius: '20px',
   },
-})
-
-export const CloseButtonContainer = styled('div', {
-  position: 'absolute',
-  top: '20px',
-  right: '20px',
-  cursor: 'pointer',
-})
-
-export const PlaceDetailsContainer = styled('div', {
-  margin: '0 2rem',
-  display: 'flex',
-  height: '50%',
-  flex: 1,
-  flexDirection: 'column',
-  justifyContent: 'space-between',
-  rowGap: '3rem',
 })
