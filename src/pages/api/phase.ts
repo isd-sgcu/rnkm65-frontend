4f579b7655fd15487c8ad706138d69b1b740f765
Thanayut T <b@thanayut.in.th>
2022-08-04 17:28:15 +0700

    Force phase to ESTAMP for preview

--- src/pages/api/phase.ts
+++ src/pages/api/phase.ts
@@ -1,14 +1,16 @@
-import { Phase } from 'common/constants/phase'
-import getPhase from 'common/utils/phase/getPhase'
+// import { Phase } from 'common/constants/phase'
+// import getPhase from 'common/utils/phase/getPhase'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  phase: Phase
+  // phase: Phase
+  phase: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ phase: getPhase(new Date()) })
+  // res.status(200).json({ phase: getPhase(new Date()) })
+  res.status(200).json({ phase: 'ESTAMP' })
 }
