40025365aa7c6ee4d3a14a4c32f611fdcc310faa
Siriwudhi Sawaidee <41264368+swsd2544@users.noreply.github.com>
2022-08-04 23:56:29 +0700

    Use estamps props as initial data for checked events

--- src/module/EStamp/index.tsx
+++ src/module/EStamp/index.tsx
@@ -29,7 +29,7 @@ const EStamp = ({ events }: EStampProps) => {
     ['events'],
     () => getAllCheckedEvents(),
     {
-      initialData: [],
+      initialData: events,
     }
   )
   useHideFooter()
