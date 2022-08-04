897d3310d5f10399df802e7046e1dbc7f27b6ecd
betich <betich365@gmail.com>
2022-08-04 15:51:58 +0700

    feat: add check in check out component & fix flipped ticket logic

--- src/module/Profile/pages/EStamp/hooks/useCheckInData/index.ts
+++ src/module/Profile/pages/EStamp/hooks/useCheckInData/index.ts
@@ -1,3 +1,4 @@
+import { Axios, AxiosResponse } from 'axios'
 import { CHECKIN_LOCATION } from 'common/constants/checkin'
 import { apiClient } from 'common/utils/axios'
 import { CheckinDTO, CheckinType } from 'dto/checkinDTO'
@@ -6,52 +7,96 @@ import { useRouter } from 'next/router'
 import { useEffect, useMemo, useState } from 'react'
 
 export const useCheckInData = () => {
+  // please excuse my code, i am terribly sorry
+
   const [isLoading, setLoading] = useState(true)
-  const [pageStatus, setPageStatus] = useState(PageType.checkin)
-  const [location, setLocation] = useState('')
-  const [token, setToken] = useState('')
-  const i18nKey = useMemo(() => PageType[pageStatus], [pageStatus])
+  const [mainEventStatus, setMainEventStatus] = useState(PageType.checkin)
+  const [mainEventToken, setMainEventToken] = useState('')
+  const [freshmenNightStatus, setFreshmenNightStatus] = useState(
+    PageType.checkin
+  )
+  const [freshmenNightToken, setFreshmenNightToken] = useState('')
+
+  const mainEvent_i18nKey = useMemo(
+    () => PageType[mainEventStatus],
+    [mainEventToken]
+  )
+  const freshmenNight_i18nKey = useMemo(
+    () => PageType[freshmenNightStatus],
+    [freshmenNightToken]
+  )
   const router = useRouter()
 
   useEffect(() => {
-    const fetchToken = async () => {
-      const { id } = router.query
-      const eventType = parseInt(id as string, 10)
-      if (!eventType) {
-        router.replace('/')
-        return
-      }
-
-      const eventPlace = CHECKIN_LOCATION[eventType]
-      if (!eventPlace) {
-        router.replace('/')
-        return
-      }
-      setLocation(eventPlace)
-
+    const fetchEvent = async (
+      eventType: string,
+      onData: (res: AxiosResponse<CheckinDTO, any>) => void,
+      onError: (err: unknown) => void
+    ) => {
       try {
+        // fetch main event
         const res = await apiClient.post<CheckinDTO>('/qr/checkin/verify', {
           event_type: eventType,
         })
-        setToken(res.data.checkin_token)
-        setPageStatus(
-          res.data.checkin_type === CheckinType.CHECK_IN
-            ? PageType.checkin
-            : PageType.checkout
-        )
+
+        onData(res)
       } catch (err) {
-        setPageStatus(PageType.error)
-      } finally {
-        setLoading(false)
+        onError(err)
       }
     }
+
+    const fetchToken = async () => {
+      // fetch both event types
+      const MainEventType = CHECKIN_LOCATION[1]
+      const FreshmenNightType = CHECKIN_LOCATION[2]
+
+      // digusting code, im sry again
+      await Promise.all([
+        fetchEvent(
+          MainEventType,
+          (res) => {
+            setMainEventToken(res.data.checkin_token)
+            setMainEventStatus(
+              res.data.checkin_type === CheckinType.CHECK_IN
+                ? PageType.checkin
+                : PageType.checkout
+            )
+          },
+          (err) => {
+            console.error('er', err)
+            setMainEventStatus(PageType.error)
+          }
+        ),
+        fetchEvent(
+          FreshmenNightType,
+          (res) => {
+            setFreshmenNightToken(res.data.checkin_token)
+            setFreshmenNightStatus(
+              res.data.checkin_type === CheckinType.CHECK_IN
+                ? PageType.checkin
+                : PageType.checkout
+            )
+          },
+          (err) => {
+            console.error('er', err)
+            setFreshmenNightStatus(PageType.error)
+          }
+        ),
+      ])
+
+      return setLoading(false)
+    }
+
     fetchToken()
   }, [router])
 
   return {
-    i18nKey,
-    pageStatus,
     isLoading,
-    location,
+    mainEvent_i18nKey,
+    mainEventStatus,
+    mainEventToken,
+    freshmenNight_i18nKey,
+    freshmenNightStatus,
+    freshmenNightToken,
   }
 }
--- src/module/Profile/pages/EStamp/index.tsx
+++ src/module/Profile/pages/EStamp/index.tsx
@@ -24,84 +24,89 @@ const EStampProfile = () => {
   // const { baan, isLoading: isBaanLoading } = useBaanData(user?.baanId)
   const { t } = useSSRTranslation('profile')
 
-  // const {
-  //   isLoading: isCheckInLoading,
-  //   i18nKey,
-  //   pageStatus,
-  //   location,
-  // } = useCheckInData()
+  const {
+    isLoading: isCheckInLoading,
+    mainEvent_i18nKey,
+    freshmenNight_i18nKey,
+  } = useCheckInData()
 
-  return (
-    <EStampProfileContainer>
-      <Typography
-        color="new-primary"
-        css={{ textAlign: 'center' }}
-        variant="h3"
-      >
-        {t('profile')}
-      </Typography>
-      <UserProfile />
-      <ActionContainer>
-        <HeaderContainer>
-          <Typography variant="subhead3" color="white">
-            {t('scanQR')}
-          </Typography>
-
-          <Link href="/eStamp?eventId=CAMERA">{t('scanQRButton')}</Link>
-        </HeaderContainer>
-
-        {/* <HeaderContainer>
-            <div>
-              <Typography variant="subhead3" color="white">
-                รับเพื่อนก้าวใหม่ : {t(`${'checkin'}Button`)}แล้ว
-              </Typography>
-
-              <Typography variant="subhead3" color="white">
-                Freshmen Night : {t(`${'checkin'}Button`)}แล้ว
-              </Typography>
-            </div>
-          </HeaderContainer> */}
-
-        <HeaderContainer>
-          <Typography variant="h4" color="white">
-            {t('eStampPage')}
-          </Typography>
-
-          <Link href="/eStamp">{t('eStampButton')}</Link>
-        </HeaderContainer>
-
-        <RedeemTicketContainer>
+  if (isCheckInLoading) return <Loading />
+  else
+    return (
+      <EStampProfileContainer>
+        <Typography
+          color="new-primary"
+          css={{ textAlign: 'center' }}
+          variant="h3"
+        >
+          {t('profile')}
+        </Typography>
+        <UserProfile />
+        <ActionContainer>
           <HeaderContainer>
             <div>
-              <Typography variant="h4" color="white">
-                {t('redeemTicket')}
-                {user?.year !== '1' && t('onlyCU106')}
+              <Typography variant="subhead3" color="white">
+                {t(`checkInCheckOut`)}
               </Typography>
 
               <Typography variant="body" color="white">
-                {t('redeemTicketInfo')}
+                Main Event : {t(`${mainEvent_i18nKey}Button`)}
+              </Typography>
+
+              <Typography variant="body" color="white">
+                Freshmen Night : {t(`${freshmenNight_i18nKey}Button`)}
               </Typography>
             </div>
-
-            {/* <Button type="button" variant="eStampProfile">
-                {t('redeemTicketButton')}
-              </Button> */}
           </HeaderContainer>
 
-          {user?.year === '1' && (
-            <TicketStatusContainer>
-              <Typography variant="h4" color="new-secondary">
-                {t('ticketStatus')}
-              </Typography>
-              <Typography variant="subhead3" color="new-primary">
-                {user?.isGotTicket ? t('unRedeemed') : t('redeemed')}
-              </Typography>
-            </TicketStatusContainer>
-          )}
-        </RedeemTicketContainer>
-      </ActionContainer>
-    </EStampProfileContainer>
-  )
+          <HeaderContainer>
+            <Typography variant="subhead3" color="white">
+              {t('scanQR')}
+            </Typography>
+
+            <Link href="/eStamp?eventId=CAMERA">{t('scanQRButton')}</Link>
+          </HeaderContainer>
+
+          <HeaderContainer>
+            <Typography variant="h4" color="white">
+              {t('eStampPage')}
+            </Typography>
+
+            <Link href="/eStamp">{t('eStampButton')}</Link>
+          </HeaderContainer>
+
+          <RedeemTicketContainer>
+            <HeaderContainer>
+              <div>
+                <Typography variant="h4" color="white">
+                  {t('redeemTicket')}
+                  {user?.year !== '1' && t('onlyCU106')}
+                </Typography>
+
+                <Typography variant="body" color="white">
+                  {t('redeemTicketInfo')}
+                </Typography>
+              </div>
+
+              {/* <Button type="button" variant="eStampProfile">
+                {t('redeemTicketButton')}
+              </Button> */}
+            </HeaderContainer>
+
+            {user?.year === '1' && (
+              <TicketStatusContainer>
+                <Typography variant="h4" color="new-secondary">
+                  {t('ticketStatus')}
+                </Typography>
+                <Typography variant="subhead3" color="new-primary">
+                  {user?.isGotTicket ? t('redeemed') : t('unRedeemed')}
+                </Typography>
+              </TicketStatusContainer>
+            )}
+          </RedeemTicketContainer>
+        </ActionContainer>
+      </EStampProfileContainer>
+    )
 }
 
 export default EStampProfile
